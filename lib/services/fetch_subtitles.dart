fd13a513035b1f056f933172fa3b644e412519c7
turalbabayev <turalbabayev@outlook.com.tr>
2025-04-26 01:42:52 +0300

    Clean repository to show only privacy policy

--- lib/services/fetch_subtitles.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firebase_database/firebase_database.dart';
-
-FirebaseDatabase _database = FirebaseDatabase.instance;
-final _ref = _database.ref();
-
-Future<DataSnapshot> fetch_subtitles(int index) {
-  return _ref.child("konular/$index/altkonular").get();
-}
